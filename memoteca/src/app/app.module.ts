33e90c045e76394b09c891ed046d5647b00c8124
Alisson Jaques - Computer Engineer <alissonjaquesrmq@gmail.com>
2023-07-25 14:58:16 -0300

    Memoteca: configurações básicas

---
+++ memoteca/src/app/app.module.ts
@@ -0,0 +1,15 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { CabecalhoComponent } from './components/cabecalho/cabecalho.component';
+import { RodapeComponent } from './components/rodape/rodape.component';
+
+@NgModule({
+  declarations: [AppComponent, CabecalhoComponent, RodapeComponent],
+  imports: [BrowserModule, AppRoutingModule],
+  providers: [],
+  bootstrap: [AppComponent],
+})
+export class AppModule {}
