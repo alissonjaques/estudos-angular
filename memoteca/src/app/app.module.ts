47710c2fc8d7de5ef9d7eb38ba85f537635b438a
Alisson Jaques - Computer Engineer <alissonjaquesrmq@gmail.com>
2023-07-25 15:58:04 -0300

    Memoteca: configuração do template de cadastro e listagem

--- memoteca/src/app/app.module.ts
+++ memoteca/src/app/app.module.ts
@@ -5,9 +5,10 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { CabecalhoComponent } from './components/cabecalho/cabecalho.component';
 import { RodapeComponent } from './components/rodape/rodape.component';
+import { CriarPensamentoComponent } from './components/pensamentos/criar-pensamento/criar-pensamento.component';
 
 @NgModule({
-  declarations: [AppComponent, CabecalhoComponent, RodapeComponent],
+  declarations: [AppComponent, CabecalhoComponent, RodapeComponent, CriarPensamentoComponent],
   imports: [BrowserModule, AppRoutingModule],
   providers: [],
   bootstrap: [AppComponent],
