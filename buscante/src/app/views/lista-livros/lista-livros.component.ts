92e34e0e55c491075edea362fe24a982f04c7aec
Alisson Jaques <alissonjaquesrmq@gmail.com>
2023-09-13 12:49:47 -0300

    buscante feat: subscrição do método buscarLivros

--- buscante/src/app/views/lista-livros/lista-livros.component.ts
+++ buscante/src/app/views/lista-livros/lista-livros.component.ts
@@ -1,20 +1,26 @@
-import { Component } from '@angular/core';
+import { Component, OnDestroy } from '@angular/core';
 import { LivroService } from 'src/app/services/livro.service';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-lista-livros',
   templateUrl: './lista-livros.component.html',
   styleUrls: ['./lista-livros.component.css']
 })
-export class ListaLivrosComponent {
+export class ListaLivrosComponent implements OnDestroy {
 
   listaLivros: [];
   campoBusca: string = '';
+  subscription: Subscription;
 
   constructor(private livroService: LivroService) { }
 
+  ngOnDestroy(): void {
+    this.subscription.unsubscribe()
+  }
+
   buscarLivros() {
-    this.livroService.buscar(this.campoBusca).subscribe({
+    this.subscription = this.livroService.buscar(this.campoBusca).subscribe({
       next: (retornoAPI) => console.log(retornoAPI),
       error: (error) => console.log(error)
     })
