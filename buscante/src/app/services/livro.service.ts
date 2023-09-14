b44c3c27a65185fdbdde006bc5bef30df2541a3a
Alisson Jaques <alissonjaquesrmq@gmail.com>
2023-09-13 22:30:19 -0300

    buscante feat: busca por livros por nome

--- buscante/src/app/services/livro.service.ts
+++ buscante/src/app/services/livro.service.ts
@@ -1,5 +1,7 @@
 import { HttpClient, HttpParams } from "@angular/common/http";
 import { Injectable } from "@angular/core";
+import { Observable, map, tap } from "rxjs";
+import { Item, LivrosResultado } from "../models/interfaces";
 
 @Injectable({
   providedIn: "root",
@@ -9,8 +11,15 @@ export class LivroService {
 
   constructor(private http: HttpClient) { }
 
-  buscar(valorDigitado: string) {
-    const params = new HttpParams().append('q', valorDigitado);
-    return this.http.get(this.API, { params });
-  }
+   buscar(valorDigitado: string): Observable<Item[]> {
+        const params = new HttpParams().append('q', valorDigitado)
+
+        return this.http.get<LivrosResultado>(this.API, { params })
+          .pipe(
+            tap(retornoAPI => console.log('Fluxo do tap', retornoAPI)), // retorna LivrosResultado
+            map(resultado => resultado.items), // realiza o filtro, selecionando apenas o campo items de livros resultados
+            tap(resultado => console.log('Fluxo após o map', resultado) // retorna Item[]
+          )
+        )
+    }
 }
--- buscante/src/app/views/lista-livros/lista-livros.component.ts
+++ buscante/src/app/views/lista-livros/lista-livros.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnDestroy } from '@angular/core';
 import { LivroService } from 'src/app/services/livro.service';
 import { Subscription } from 'rxjs';
+import { Item, Livro } from 'src/app/models/interfaces';
+import { LivroVolumeInfo } from 'src/app/models/LivroVolumeInfo';
 
 @Component({
   selector: 'app-lista-livros',
@@ -9,7 +11,7 @@ import { Subscription } from 'rxjs';
 })
 export class ListaLivrosComponent implements OnDestroy {
 
-  listaLivros: [];
+  listaLivros: Livro[];
   campoBusca: string = '';
   subscription: Subscription;
 
@@ -21,9 +23,16 @@ export class ListaLivrosComponent implements OnDestroy {
 
   buscarLivros() {
     this.subscription = this.livroService.buscar(this.campoBusca).subscribe({
-      next: (retornoAPI) => console.log(retornoAPI),
+      next: (items) => {
+        this.listaLivros = this.livrosResultadoParaLivros(items)},
       error: (error) => console.log(error)
     })
+  }
+
+  livrosResultadoParaLivros(items: Item[]): LivroVolumeInfo[] {
+    return items.map((item) => {
+      return new LivroVolumeInfo(item);
+    });
   }
 }
 
