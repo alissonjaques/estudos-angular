9b57c38536197f3c5692bdbb8f593cb294e21b16
Alisson Jaques <alissonjaquesrmq@gmail.com>
2023-09-18 13:09:42 -0300

    animacoes refactor: melhoria nos triggers das animações

--- animacoes/src/app/animations.ts
+++ animacoes/src/app/animations.ts
@@ -1,27 +1,46 @@
 import { animate, state, style, transition, trigger } from '@angular/animations';
 
 export const highlightedStateTrigger = trigger(
-      'highlightedState',
+  'highlightedState',
+  [
+    state('default', style({ border: '2px solid #B2B6FF' })),
+    state(
+     'highlighted',
+      style({
+        border: '4px solid #B2B6FF',
+        filter: 'brightness(92%)'
+      })
+    ),
+    transition(
+     'default => highlighted',
       [
-        state('default', style({ border: '2px solid #B2B6FF' })),
-        state(
-          'highlighted',
+        animate(
+          '200ms ease-out',
           style({
-            border: '4px solid #B2B6FF',
-            filter: 'brightness(92%)'
+            transform: 'scale(1.02)'
           })
         ),
-        transition(
-          'default => highlighted',
-          [
-            animate(
-              '200ms ease-out',
-              style({
-                transform: 'scale(1.02)'
-              })
-            ),
-            animate(200)
-          ]
-        )
+        animate(200)
       ]
     )
+  ]
+)
+
+export const shownStateTrigger = trigger(
+  'shownState',
+  [
+    transition(
+      ':enter',
+      [
+        style({ opacity: 0 }),
+        animate(300, style({ opacity: 1 }))
+      ]
+    ),
+    transition(
+      ':leave',
+      [
+        animate(300, style({ opacity: 0 }))
+      ]
+    )
+  ]
+)
--- animacoes/src/app/lista-tarefas/lista-tarefas.component.ts
+++ animacoes/src/app/lista-tarefas/lista-tarefas.component.ts
@@ -4,13 +4,13 @@ import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 import { TarefaService } from 'src/app/service/tarefa.service';
 import { Tarefa } from '../interface/tarefa';
-import { highlightedStateTrigger } from '../animations';
+import { highlightedStateTrigger, shownStateTrigger } from '../animations';
 
 @Component({
   selector: 'app-lista-tarefas',
   templateUrl: './lista-tarefas.component.html',
   styleUrls: ['./lista-tarefas.component.css'],
-  animations: [highlightedStateTrigger]
+  animations: [highlightedStateTrigger, shownStateTrigger]
 })
 export class ListaTarefasComponent implements OnInit {
   listaTarefas: Tarefa[] = [];
